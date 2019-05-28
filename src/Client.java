0c2a753b93957c91e898129ec5cb8f67b9e4806c
oksanahl <oksana.hlushko.canada@gmail.com>
2019-05-27 23:17:42 -0700

    added code that allows to parse json array

--- src/Client.java
+++ src/Client.java
@@ -3,6 +3,12 @@ import java.io.*;
 import java.util.Scanner;
 import java.util.Vector;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
 public class Client {
 
     // initialize socket and input output streams
@@ -35,13 +41,72 @@ public class Client {
 
     }
 
-    public Vector<String> getCurrentMachineList() throws IOException {
+    public Vector<String> getCurrentMachineList() throws IOException, ParseException {
 
         Vector<String> responseList = new Vector<>();
 
         System.out.println("Finding active machines: "); //change to log
 
-        BufferedReader reader = new BufferedReader(new FileReader("machinelist.txt"));
+        JSONParser parser = new JSONParser();
+        try
+        {
+            Object object = parser
+                    .parse(new FileReader("machinelistone.json"));
+
+            //convert Object to JSONObject
+            JSONObject jsonObject = (JSONObject)object;
+
+            //Reading the array
+            JSONArray ipAddresses = (JSONArray)jsonObject.get("ipAddress");
+            JSONArray statuses = (JSONArray)jsonObject.get("status");
+            JSONArray gateways = (JSONArray)jsonObject.get("gateway");
+            //Printing all the values
+
+            System.out.println("ipAddresses:");
+
+            for(Object ipAddress : ipAddresses)
+            {
+                System.out.println("\t"+ipAddress.toString());
+            }
+
+            System.out.println("statuses:");
+            for(Object status : statuses){
+
+                System.out.println("\t"+status.toString());
+            }
+
+            System.out.println("gateways:");
+            for(Object gateway : gateways){
+
+                System.out.println("\t"+gateway.toString());
+            }
+
+        }
+        catch(FileNotFoundException fe)
+        {
+            fe.printStackTrace();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+
+        // parsing file "JSONExample.json"
+        //Object obj = new JSONParser().parse(new FileReader("machinelist.json"));
+
+        // typecasting obj to JSONObject
+        //JSONObject jo = (JSONObject) obj;
+
+       /* // getting firstName and lastName
+        String firstName = (String) jo.get("firstName");
+        String lastName = (String) jo.get("lastName");
+
+        System.out.println(firstName);
+        System.out.println(lastName);*/
+
+
+        /*BufferedReader reader = new BufferedReader(new FileReader("machinelist.json"));
 
         String st;
         while ((st = reader.readLine()) != null) {
@@ -51,7 +116,7 @@ public class Client {
             }
         }
 
-        System.out.println("Found " + responseList.size() + " " + "machines"); //change to log
+        System.out.println("Found " + responseList.size() + " " + "machines"); //change to log*/
 
         return responseList;
 
@@ -65,19 +130,19 @@ public class Client {
         System.out.println("Machine" + " " + ipAddress +" will be terminated" );
     }
 
-    public static void main(String args[]) throws IOException {
+    public static void main(String args[]) throws IOException, ParseException {
 
         if(args.length == 0) {
         Client client = new Client("localhost",59898);
         client.getCurrentMachineList();}
 
-        if(args[0] == "Stop") {
-            Client client1 = new Client("localhost",5000);
-            client1.sendStopMachine();
+        //if(args[0] == "Stop") {
+            //Client client1 = new Client("localhost",5000);
+            //client1.sendStopMachine();
 
 
 
-    }}}
+    }}//}
 
         /*
         // close the connection
