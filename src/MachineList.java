159d80b2e9bf2ca5e17fb957a7ae7cf596388187
oksanahl <oksana.hlushko.canada@gmail.com>
2019-05-29 09:20:39 -0700

    changed to arraylist

--- src/MachineList.java
+++ src/MachineList.java
@@ -6,15 +6,16 @@ public class MachineList {
 
     public static void main(String[] args) {
 
-        Vector<Object> listStrings = new Vector<Object>();
+        ArrayList<Object> machinelist = new ArrayList<Object>();
 
-        Machine machineone = new Machine("172.34.56.78", 174, true, false);
-        Machine machinetwo = new Machine("172.35.56.78", 175, true, false);
-        listStrings.add(machineone);
-        listStrings.add(machinetwo);
+        Machine machine = new Machine("172.34.56.78", 174, true, false);
+        Machine machine1 = new Machine("172.35.56.78", 175, true, false);
+        machinelist.add("machine");
+        machinelist.add("machine1");
 
-        System.out.println(listStrings.size());
-        System.out.println("Vector: " + listStrings);
+        System.out.println(machinelist.size());
+        System.out.println("Array: " + machinelist);
+
 
 
     }
